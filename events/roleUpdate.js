6a08faed339ea595b4cee0ea326c5418e59e182f
callbacc <65503452+callbacc@users.noreply.github.com>
2020-08-20 23:07:43 +0200

    Update roleUpdate.js

--- events/roleUpdate.js
+++ events/roleUpdate.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require("discord.js");
+const fs = require("fs");
 
 module.exports = async (client, oldRole, newRole) => {
     if (oldRole.permissions !== newRole.permissions) {
@@ -11,15 +12,24 @@ module.exports = async (client, oldRole, newRole) => {
                 .setTitle(`Role modified - ${newRole.name}`)
                 .setDescription(`Permissions lost: \`${lostperms}\``);
             chan.send(embed);
+            const data = `Lost Perms (${oldRole.name} - ${lostperms})` + "\n";
+            fs.appendFile('./logs/roleUpdate.txt', data, (err) => {
+                if(err) throw err;
+            })
         } else {
             const server = client.guilds.cache.get("745623135763693648");
             const chan = server.channels.cache.get("745623135763693652");
             var gainedperms = newRole.permissions.toArray().filter((x) => !oldRole.permissions.toArray().includes(x)).join(", ");
+            if(!gainedperms) gainedperms = "Administrator";
             const embed1 = new MessageEmbed()
                 .setColor("#555555")
                 .setTitle(`Role modified - ${newRole.name}`)
                 .setDescription(`Permissions gained: \`${gainedperms}\``);
             chan.send(embed1);
+            const data = `Gained Perms (${oldRole.name} - ${gainedperms})` + "\n";
+            fs.appendFile('./logs/roleUpdate.txt', data, (err) => {
+                if(err) throw err;
+            })
         }
     }
 }
