6b391f05e938f333f1340603093a05c59cc98062
callbacc <65503452+callbacc@users.noreply.github.com>
2020-08-20 21:33:02 +0200

    Update userUpdate.js

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -6,12 +6,26 @@ module.exports = async (client, oUser, nUser) => {
         const discrim = new MessageEmbed()
             .setColor("#03fc0f")
             .setTitle(`Discriminator updated - ${oUser.username}`)
-            .setDescription(`Old Discriminator - *${oUser.discriminator}*
-            New Discriminator - *${nUser.discriminator}*`);
+            .setDescription(`Old Discriminator - ***${oUser.discriminator}***
+            New Discriminator - ***${nUser.discriminator}***`);
         const server = client.guilds.cache.get("745623135763693648");
         const chan = server.channels.cache.get("745623135763693652");
         chan.send(discrim);
-        const data = `${oUser.username} - ${oUser.discriminator} - ${nUser.discriminator}` + "\n";
+        const data = `Discriminator (${oUser.username} - ${oUser.discriminator} - ${nUser.discriminator})` + "\n";
+        fs.appendFile('./logs/userUpdate.txt', data, (err) => {
+            if(err) throw err;
+        })
+    }
+    if(oUser.username !== nUser.username) {
+        const uName = new MessageEmbed()
+            .setColor("#03fc0f")
+            .setTitle(`Username updated - ${oUser.username}`)
+            .setDescription(`Old Username - ***${oUser.username}***
+            New Username - ***${nUser.username}***`);
+        const server = client.guilds.cache.get("745623135763693648");
+        const chan = server.channels.cache.get("745623135763693652");
+        chan.send(uName);
+        const data = `Username (${oUser.username} - ${nUser.username})` + "\n";
         fs.appendFile('./logs/userUpdate.txt', data, (err) => {
             if(err) throw err;
         })
