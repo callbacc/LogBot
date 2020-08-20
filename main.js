da82642c3bbbb6b2770bb94f3bde222823a5a8da
callbacc <65503452+callbacc@users.noreply.github.com>
2020-08-20 19:48:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,17 @@
 const { Client, Collection } = require("discord.js");
 // Getting the config file.
 const config = require("./config.json");
+// Requiring fs
+const fs = require("fs");
 // Making our client
 const client = new Client();
+// Defining the commands and the aliases
+["commands", "aliases"].forEach(x => client[x] = new Collection());
+// Defining the categories.
+client.categories = fs.readdirSync("./commands/");
+// Getting the message and event handler.
+["command", "event"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
 // Starting the bot using the token from the config file.
 client.login(config.token);
