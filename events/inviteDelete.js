fdb61822428a998eaec1d8bfbd8d8312c08c8466
callbacc <65503452+callbacc@users.noreply.github.com>
2020-08-21 16:52:27 +0200

    Create inviteDelete.js

---
+++ events/inviteDelete.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = (client, invite) => {
+    const server = client.guilds.cache.get("745623135763693648");
+    const chan = server.channels.cache.get("745623135763693652");
+    const embed = new MessageEmbed()
+        .setColor("#8B0000")
+        .setTitle(`Invite deleted`)
+        .setDescription(`Invite code: ${invite.code}`);
+    chan.send(embed);
+}
