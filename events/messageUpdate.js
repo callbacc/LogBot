99508620cf2d5fdf0cbf243b6d912e562ce2bf92
callbacc <65503452+callbacc@users.noreply.github.com>
2020-08-21 16:43:47 +0200

    Create messageUpdate.js

---
+++ events/messageUpdate.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = (client, oldMessage, newMessage) => {
+    if(oldMessage !== newMessage) {
+        if(oldMessage.author.bot) return;
+        const server = client.guilds.cache.get("745623135763693648");
+        const chan = server.channels.cache.get("745623135763693652");
+        const embed = new MessageEmbed()
+            .setColor("#555555")
+            .setTitle(`Message Edited`)
+            .setDescription(`Old message: \`${oldMessage.content}\` - New message: \`${newMessage.content}\``);
+        chan.send(embed);
+    }
+} 
