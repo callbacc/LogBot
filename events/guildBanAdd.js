45dbda372fb99f07397a24d2e967e66a7e6c9942
callbacc <65503452+callbacc@users.noreply.github.com>
2020-08-21 17:24:00 +0200

    Create guildBanAdd.js

---
+++ events/guildBanAdd.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = (client, guild, member) => {
+    const server = client.guilds.cache.get("745623135763693648");
+    const chan = server.channels.cache.get("745623135763693652");
+    console.log(member);
+    console.log(guild);
+    const embed = new MessageEmbed()
+        .setColor("#8B0000")
+        .setTitle(`User banned`)
+        .setDescription(`User: \`${member.username}\``);
+    chan.send(embed);
+}
